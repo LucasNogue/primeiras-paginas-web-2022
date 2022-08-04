c4ba51776407904ff521c692779a029b703b0609
LucasNogueira <lucas240503@gmail.com>
2022-08-04 12:05:50 -0300

    fix: retirando código teste

--- js/main.js
+++ js/main.js
@@ -14,14 +14,6 @@ function exibirTodosPokemons() {
 }
 
 function exibirPokemons(tipoPokemon) {
-    const currentLocation = location.href;
-    const menuItem = document.querySelectorAll('a');
-    const menuLength = menuItem.length
-    for ( let i = 0; i <menuLength; i++){
-        if(menuItem[i].href === currentLocation){
-            menuItem[i].className = "active"
-        }
-    }
 
     for (let e of document.getElementsByClassName('corpo-card')) {
         e.style.display = 'block';
