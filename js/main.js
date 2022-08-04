5f01bdf6792b3a500d41c45706906c9321b7307d
LucasNogueira <lucas240503@gmail.com>
2022-08-04 15:12:59 -0300

    feat: adicionando indicação de guia selecionada

--- js/main.js
+++ js/main.js
@@ -7,7 +7,22 @@ function trocarPokebola(classe) {
        
 }
 
-function exibirTodosPokemons() {
+function destacarLink(tipoPokemon) {
+    let listaLink = ['fire','water','grass','electric','todos']; 
+    let tamanhoListaLink = listaLink.length;
+    for(i=0; i < tamanhoListaLink; i++){
+        document.getElementById("link-"+listaLink[i]).classList.remove("link-"+listaLink[i]+"-ativo");
+
+    }
+
+    let linkSelecionado = document.querySelector("#link-"+tipoPokemon);
+    linkSelecionado.classList.add ("link-"+tipoPokemon+"-ativo");
+
+}
+
+function exibirTodosPokemons(tipoPokemon) {
+
+    destacarLink(tipoPokemon);
 
     for (let e of document.getElementsByClassName('corpo-card')) {
         e.style.display = 'block';
@@ -17,14 +32,16 @@ function exibirTodosPokemons() {
 
 function exibirPokemons(tipoPokemon) {
 
+    let listaTipos = ['fire','water','grass','electric']; 
+    let listaTipoNaoSelecionado = [];
+
+    destacarLink(tipoPokemon);
+
     for (let e of document.getElementsByClassName('corpo-card')) {
         e.style.display = 'block';
 
     }
    
-    let listaTipos = ['fire','water','grass','electric']; 
-    let listaTipoNaoSelecionado = [];
-
     for(i=0;i <= 3;i++){
         if(tipoPokemon !==listaTipos[i]){
             listaTipoNaoSelecionado.push(listaTipos[i]);
@@ -35,7 +52,7 @@ function exibirPokemons(tipoPokemon) {
     for(i=0; i <= 3;i++) {
         for (let e of document.getElementsByClassName(listaTipoNaoSelecionado[i])) {
             e.style.display = 'none';
-            
+        
         }
     }
 }
